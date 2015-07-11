a09b35b621b00550ce71bf4deeddf357dc544ef0
qwerboo <abc123jjb@163.com>
2015-07-11 18:40:32 +0800

    Update readwrite.h

--- readwrite.h
+++ readwrite.h
@@ -1,11 +1,5 @@
 #include <unistd.h>  
-#include <sys/types.h>  
-#include <sys/socket.h>  
-#include <netinet/in.h>  
-#include <arpa/inet.h>  
-#include <stdlib.h>  
 #include <stdio.h>  
-#include <errno.h>  
 #define MAXLEN 1024 
 
 struct packet  
